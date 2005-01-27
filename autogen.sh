34ece938b7a833ff88eb096eb3e0aedc00e30575
Benedikt Meurer <benny@xfce.org>
2005-01-27 09:20:47 +0000

    2005-01-27      Benedikt Meurer <benny@xfce.org>
    	* autogen.sh, scripts/xdt-autogen.in: Don't run the configure script
    	  if the environment variable NOCONFIGURE is set.
    
    
    (Old svn revision: 2267)

--- autogen.sh
+++ autogen.sh
@@ -13,9 +13,14 @@ if (type xdt-autogen) >/dev/null 2>&1; then
 else
   (aclocal &&
    automake --add-missing --copy --gnu &&
-   autoconf &&
-   ./configure --enable-maintainer-mode $@ &&
-   echo "Now type \"make\" to build.") || exit 1
+   autoconf) || exit 1
+
+  if test x"${NOCONFIGURE}" = x""; then
+    (./configure --enable-maintainer-mode $@ &&
+     echo "Now type \"make\" to build.") || exit 1
+  else
+    echo "Skipping configure process."
+  fi
 fi
 
 # vi:set ts=2 sw=2 et ai:
