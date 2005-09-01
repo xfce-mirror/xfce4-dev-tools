b21d516509b2f1d4cb296ba07065f6a5b94a25e0
Benedikt Meurer <benny@xfce.org>
2005-09-01 15:07:16 +0000

    2005-09-01      Benedikt Meurer <benny@xfce.org>
    	* autogen.sh: Don't depend on xfce4-dev-tools in autogen.sh.
    
    
    
    
    (Old svn revision: 17247)

--- autogen.sh
+++ autogen.sh
@@ -8,21 +8,24 @@
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
 #
 
-(type xdt-autogen) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: You don't seem to have the Xfce development tools installed on
-            your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
-EOF
-  exit 1
-}
-
 # substitute revision and date
 revision=`svn info $0 | awk '/^Revision: / {printf "%04d\n", $2}'`
 sed -e "s/@DATE@/`date +%Y%m%d`/g" -e "s/@REVISION@/${revision}/g" \
   < "configure.in.in" > "configure.in"
 
-exec xdt-autogen $@
+if (type xdt-autogen) >/dev/null 2>&1; then
+  exec xdt-autogen $@
+else
+  (aclocal &&
+   automake --add-missing --copy --gnu &&
+   autoconf) || exit 1
+
+  if test x"${NOCONFIGURE}" = x""; then
+    (./configure --enable-maintainer-mode $@ &&
+     echo "Now type \"make\" to build.") || exit 1
+  else
+    echo "Skipping configure process."
+  fi
+fi
 
 # vi:set ts=2 sw=2 et ai:
