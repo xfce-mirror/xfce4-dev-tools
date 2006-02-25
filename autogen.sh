0329b83b9001f1047a925b4a4cbf8dd32f8457e0
Daichi Kawahata <daichi.k@aioros.ocn.ne.jp>
2006-02-25 06:00:16 +0000

    Fixed NULL revision string for certain locales.
    (Old svn revision: 20044)

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@
 #
 
 # substitute revision and date
-revision=`svn info $0 | awk '/^Revision: / {printf "%04d\n", $2}'`
+revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
 sed -e "s/@DATE@/`date +%Y%m%d`/g" -e "s/@REVISION@/${revision}/g" \
   < "configure.in.in" > "configure.in"
 
