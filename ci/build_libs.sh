4531a96c9a77ab225aaeb472a7c7642e2e7dcb38
Natanael Copa <ncopa@alpinelinux.org>
2020-05-26 12:22:03 +0200

    ci/build_libs.sh: update configure options
    Remove --build and --host since they are only needed for crosscompile.
    Native builds don't need those and they will not work in case someone
    tries build on 32bit.
    
    Also add envronment variables for libdir and libexecdir so the
    debian/ubuntu specific lib(exec)dir can be overridden.

--- ci/build_libs.sh
+++ ci/build_libs.sh
@@ -2,9 +2,19 @@
 
 XFCE_BASE=https://gitlab.xfce.org
 
-AUTOGEN_OPTIONS="--disable-debug --enable-maintainer-mode --host=x86_64-linux-gnu
-                --build=x86_64-linux-gnu --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
-                --libexecdir=/usr/lib/x86_64-linux-gnu --sysconfdir=/etc --localstatedir=/var --enable-gtk-doc"
+: ${libdir:="/usr/lib/x86_64-linux-gnu"}
+: ${libexecdir:="/usr/lib/x86_64-linux-gnu"}
+
+AUTOGEN_OPTIONS="
+  --disable-debug
+  --enable-maintainer-mode
+  --prefix=/usr
+  --libdir=$libdir
+  --libexecdir=$libexecdir
+  --sysconfdir=/etc
+  --localstatedir=/var
+  --enable-gtk-doc
+"
 
 # list of git repos in build order
 REPOS="${XFCE_BASE}/xfce/xfce4-dev-tools.git
