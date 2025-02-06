xfce-mirror/xfce4-dev-tools

commit b3503511779cbef7f95311c51daa651ba75dc2a9

ci: Use same gtk-doc detection for container and project build

ci/build_libs.sh
@@ -52,8 +52,7 @@ for URL in ${REPOS}; do
     if [ -f meson.build ]; then
       BUILDDIR_PREFIX=build
       # Passing unknown options to 'meson setup' is a fatal error
-      if ( [ -f meson_options.txt ] && grep -q "'gtk-doc'" meson_options.txt ) || \
-         ( [ -f meson.options ] && grep -q "'gtk-doc'" meson.options );
+      if meson configure 2>/dev/null | grep -q '\<gtk-doc\>'
       then
         GTK_DOC_OPT='-Dgtk-doc=true'
       else

commit 3694904964ad1eefe3d7386e473d2e234191ce51

ci: Fix random pipefail in container build
Fixes: b3503511779cbef7f95311c51daa651ba75dc2a9
Closes: #68

ci/build_libs.sh
@@ -52,7 +52,7 @@ for URL in ${REPOS}; do
     if [ -f meson.build ]; then
       BUILDDIR_PREFIX=build
       # Passing unknown options to 'meson setup' is a fatal error
-      if meson configure 2>/dev/null | grep -q '\<gtk-doc\>'
+      if meson configure 2>/dev/null | grep '\<gtk-doc\>' >/dev/null
       then
         GTK_DOC_OPT='-Dgtk-doc=true'
       else